34d779546cb6f6f4132b13c3512306d7a80f2a12
Juan Peruzzo <juanperuzzo13@gmail.com>
2026-01-04 22:06:56 -0300

    Initial commit

---
+++ src/test/java/com/juanperuzzo/flappynaruu/FlappynaruuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanperuzzo.flappynaruu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlappynaruuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
