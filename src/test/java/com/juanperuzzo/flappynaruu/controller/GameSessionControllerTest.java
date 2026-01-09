a572d609e20a555c6b0fa1ddef93c3c5347c8f53
Juan Peruzzo <juanperuzzo13@gmail.com>
2026-01-09 11:35:01 -0300

    test: add GameSessionController WebMvc tests

---
+++ src/test/java/com/juanperuzzo/flappynaruu/controller/GameSessionControllerTest.java
@@ -0,0 +1,52 @@
+package com.juanperuzzo.flappynaruu.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.juanperuzzo.flappynaruu.controller.request.SaveScoreRequest;
+import com.juanperuzzo.flappynaruu.service.GameSessionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(GameSessionController.class)
+public class GameSessionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GameSessionService service;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void shouldReturn201WhenScoreIsHighscore() throws Exception {
+        SaveScoreRequest request = new SaveScoreRequest("Uzzoper", 10);
+
+        when(service.registerScoreIfHighscore("Uzzoper", 10)).thenReturn(true);
+
+        mockMvc.perform(post("/leaderboard")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        ).andExpect(status().isCreated());
+    }
+
+    @Test
+    public void shouldReturn204WhenScoreIsNotHighscore() throws Exception {
+        SaveScoreRequest request = new SaveScoreRequest("Uzzoper", 1);
+
+        when(service.registerScoreIfHighscore("Uzzoper", 1)).thenReturn(false);
+
+        mockMvc.perform(post("/leaderboard")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        ).andExpect(status().isNoContent());
+    }
+}
