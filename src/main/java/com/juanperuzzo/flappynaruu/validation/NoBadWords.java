2b6dec8bf18962716ea597bef2c8b0d47a1f41c9
Juan Peruzzo <juanperuzzo13@gmail.com>
2026-01-26 10:05:37 -0300

    feat: add custom bad words validation using jakarta constraint

---
+++ src/main/java/com/juanperuzzo/flappynaruu/validation/NoBadWords.java
@@ -0,0 +1,19 @@
+package com.juanperuzzo.flappynaruu.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = BadWordsValidator.class)
+public @interface NoBadWords {
+
+        String message() default "Nickname cannot contain bad words.";
+        Class<?>[] groups() default {};
+        Class<? extends Payload>[] payload() default {};
+}
