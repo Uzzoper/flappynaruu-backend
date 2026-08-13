234fd86d98a193bdeda5555d0e6c494cfed1e41a
Juan Peruzzo <juanperuzzo13@gmail.com>
2026-08-13 10:42:46 -0300

    refactor: replace deprecated MockBean with MockitoBean in tests

--- src/test/java/com/juanperuzzo/flappynaruu/controller/RateLimitFilterTest.java
+++ src/test/java/com/juanperuzzo/flappynaruu/controller/RateLimitFilterTest.java
@@ -6,7 +6,7 @@ import com.juanperuzzo.flappynaruu.service.GameSessionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
@@ -27,7 +27,7 @@ public class RateLimitFilterTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private GameSessionService service;
 
     @Test
